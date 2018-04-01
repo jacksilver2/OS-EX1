b07f3d36f3705660b57d3b9e04e7f841310942e0
jacksilver <jacksilver2@gmail.com>
2018-04-01 22:12:16 +0300

    test it yo

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main(unsigned int iterations)
 	printf("%.6lf nano-seconds elapsed\n", osm_operation_time(iterations));
 	printf("%.6lf nano-seconds elapsed\n", osm_function_time(iterations));
 	printf("%.6lf nano-seconds elapsed\n", osm_syscall_time(iterations));
+	//Test that git yo
 	return 0;
 }
